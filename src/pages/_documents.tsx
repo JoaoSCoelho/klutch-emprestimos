8e315ea32d568fa78f4b776fa4fb4112737abf9b
João Victor <joaovictor-san-co@hotmail.com>
2021-05-28 12:20:40 -0300

    create _document for set page metadata

---
+++ src/pages/_documents.tsx
@@ -0,0 +1,27 @@
+import Document, { Head, Html, NextScript, Main } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta name="theme-color" content="#ffffff" />
+
+          <meta name="author" content="João Victor" />
+
+          <meta property="og:title" content="Empréstimos | Klutch" />
+          <meta property="og:locale" content="pt_BR" />
+          <meta property="og:site_name" content="Klutch" />
+          <meta property="og:type" content="website" />
+          <meta name="twitter:title" content="Empréstimos | Klutch" />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
