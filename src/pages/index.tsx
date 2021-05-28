3a921cb9218b1bd7df258981194bcf6fbadf8004
João Victor <joaovictor-san-co@hotmail.com>
2021-05-28 12:29:19 -0300

    Change page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Empréstimos | Klutch</title>
       </Head>
 
       <main className={styles.main}>
