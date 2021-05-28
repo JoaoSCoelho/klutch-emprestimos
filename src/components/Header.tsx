77efa1ac859931259499207c55f7b08faadf7189
João Victor <joaovictor-san-co@hotmail.com>
2021-05-28 12:23:04 -0300

    create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import styles from '../styles/components/Header.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
+
+export function Header() {
+  /* Variável do estado para saber se o menu lateral está aberto */
+  const [openMenu, setOpenMenu] = useState<boolean>()
+
+  return (
+    <header className={styles.container}>
+      {/* Botão de menu à esquerda do cabeçalho */}
+      <button
+        className={`${styles.sandwichMenuButton} ${openMenu && styles.open}`}
+        type="button"
+        title="Abrir menu"
+        onClick={() => setOpenMenu(!openMenu)}
+      >
+        <svg viewBox="0 0 45 30" width="45" height="30">
+          <rect x="0" y="0" width="45" height="5" fill="white" />
+          <rect x="0" y="12.5" width="45" height="5" fill="white" />
+          <rect x="0" y="25" width="45" height="5" fill="white" />
+        </svg>
+      </button>
+
+      {/* Logo da Klutch que linka para página inicial */}
+      <Link href="/">
+        <a target="_self" title="Página inicial">
+          <Image src="/images/logo-klutch-white.svg" width={120} height={30} title="Klutch Tecnologia" />
+        </a>
+      </Link>
+    </header>
+  )
+}
