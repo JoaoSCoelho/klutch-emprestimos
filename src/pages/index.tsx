45894c669427dd1f8ca5f47ac77591c35d6c132c
João Victor <joaovictor-san-co@hotmail.com>
2021-05-29 09:03:27 -0300

    Update Home
    Add form to select rateTable and installment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,17 @@ import Head from 'next/head'
 import { useState } from 'react'
 import { InputMoney } from '../components/InputMoney'
 import { Subject } from '../components/Subject'
-import styles from '../styles/pages/Home.module.css'
 import api from '../services/api.json'
-import { RateTable } from '../components/RateTable'
+import { IInstallment, ITable, RateTable } from '../components/RateTable'
+import styles from '../styles/pages/Home.module.css'
 import { Button } from '../components/Button'
+import Link from 'next/link'
 
 export default function Home() {
   const [desiredValue, setDesiredValue] = useState<number>();
-  const [showTable, setShowTable] = useState<boolean>(false);
+  const [showTables, setShowTables] = useState<boolean>(false);
+  const [selectedTable, setSelectedTable] = useState<ITable>();
+  const [selectedInstallment, setSelectedInstallment] = useState<IInstallment>()
 
   return (
     <div className={styles.container}>
@@ -20,9 +23,10 @@ export default function Home() {
       <main className={styles.main}>
         <Subject text="Simulação de Taxas" plusIcon />
 
+
         <form className={styles.desiredValue} onSubmit={(e) => {
           e.preventDefault();
-          setShowTable(true);
+          setShowTables(true);
         }}>
           <strong className={styles.title}>Valor Desejado</strong>
 
@@ -31,8 +35,49 @@ export default function Home() {
           <Button text="Calcular" type="submit" />
         </form>
 
-        {showTable && (
-          <RateTable {...api.rateTable[0]} className={`${styles.rateTable}`} />
+
+        {showTables && (
+          <form className={styles.desiredTableAndInstallment}>
+            {api.rateTable.map((rateTable) => (
+              <div className={styles.inputContainer} key={rateTable.id}>
+                <input
+                  type="radio"
+                  name='rate-table'
+                  id={rateTable.id + ''}
+                  value={rateTable.id}
+                  onChange={(e) => e.target.checked && setSelectedTable(rateTable)}
+                />
+
+                <label htmlFor={rateTable.id + ''}>
+                  <RateTable
+                    disabled={selectedTable?.id !== rateTable.id}
+                    {...rateTable}
+                    className={`${styles.rateTable}`}
+                    onSelect={(installmentID) => setSelectedInstallment(rateTable.installments.find((installment) => installment.id === installmentID))}
+                    selectedInstallmentID={selectedInstallment?.id}
+                  />
+                </label>
+              </div>
+            ))}
+
+            <footer className={styles.tableAndInstallmentInfo}>
+              <div className={styles.content}>
+                <div className={styles.info}>
+                  <div className={styles.tableName}>Nome: {selectedTable?.name}</div>
+                  <div className={styles.installments}>Parcelas: {selectedInstallment?.installments || ''}</div>
+                  <div className={styles.installmentValue}>
+                    Valor da Parcela: {selectedInstallment && `R$${selectedInstallment?.installmentValue.toFixed(2).replace('.', ',')}`}
+                  </div>
+                </div>
+
+                <Link href={`/search-client?rate-table=${selectedTable?.id}&installment=${selectedInstallment?.id}`}>
+                  <a rel="next" target="_self">
+                    <Button text="Avançar" type="submit" />
+                  </a>
+                </Link>
+              </div>
+            </footer>
+          </form>
         )}
       </main>
     </div>
