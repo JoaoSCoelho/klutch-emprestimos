349d72a8610999c0f82333d45d28dc6d528fcfcb
João Victor <joaovictor-san-co@hotmail.com>
2021-05-30 13:23:42 -0300

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,23 +1,25 @@
-import { MouseEvent } from 'react'
+import { MouseEvent, ReactNode } from 'react'
 import styles from '../styles/components/Button.module.css'
 
 export interface IButtonProps {
-  text: string;
+  text?: string;
   type: 'submit' | 'button' | 'reset';
   color: 'orange' | 'blue';
   disabled?: boolean;
   onClick?: (e: MouseEvent<HTMLButtonElement>) => any;
+  className?: string;
+  children?: ReactNode
 }
 
-export function Button({ text, type, color, disabled, onClick }: IButtonProps) {
+export function Button({ text, type, color, disabled, onClick, className, children }: IButtonProps) {
   return (
     <button
       onClick={(e) => onClick && onClick(e)}
       type={type}
       disabled={disabled}
-      className={`${styles.container} ${styles[color]}`}
+      className={`${styles.container} ${styles[color]} ${className}`}
     >
-      {text}
+      {children || text}
     </button>
   )
 }
