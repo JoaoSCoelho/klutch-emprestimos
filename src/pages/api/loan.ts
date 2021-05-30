18501bfe6cc321619fd92d8ab36753eac29e502a
João Victor <joaovictor-san-co@hotmail.com>
2021-05-30 17:40:54 -0300

    Update loan.ts

--- src/pages/api/loan.ts
+++ src/pages/api/loan.ts
@@ -4,62 +4,60 @@ import api from '../../services/api.json'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
+    const {
+      clientId,
+      installmentInterest,
+      installmentInterestValue,
+      comission,
+      comissionValue,
+      cardExpiration,
+      installmentValue,
+      cardNumber,
+      cardCvc,
+      desiredValue,
+      totalLoan,
+      installmentId,
+      rateTableId,
+      contractType
+    } = req.body;
 
+    if (
+      !clientId ||
+      !installmentInterest ||
+      !installmentInterestValue ||
+      !comission ||
+      !comissionValue ||
+      !cardExpiration ||
+      !installmentValue ||
+      !cardNumber ||
+      !cardCvc ||
+      !desiredValue ||
+      !totalLoan ||
+      !installmentId ||
+      !rateTableId ||
+      !contractType
+    ) return res.status(400).send({ message: "Missing props!" })
+
+    api.solicitations.push({
+      id: api.solicitations.length + 1,
+      clientId,
+      installmentInterest,
+      installmentInterestValue,
+      comission,
+      comissionValue,
+      cardExpiration,
+      installmentValue,
+      cardNumber,
+      cardCvc,
+      desiredValue,
+      totalLoan,
+      installmentId,
+      rateTableId,
+      contractType
+    })
+
+    fs.writeFileSync('./src/services/api.json', JSON.stringify(api, null, 2))
+
+    res.status(201).json({ solicitationID: api.solicitations.length })
   } else return res.status(404).send({ message: 'Not found' });
-
-  const {
-    clientId,
-    installmentInterest,
-    installmentInterestValue,
-    comission,
-    comissionValue,
-    cardExpiration,
-    installmentValue,
-    cardNumber,
-    cardCvc,
-    desiredValue,
-    totalLoan,
-    installmentId,
-    rateTableId,
-    contractType
-  } = req.body;
-
-  if (
-    !clientId ||
-    !installmentInterest ||
-    !installmentInterestValue ||
-    !comission ||
-    !comissionValue ||
-    !cardExpiration ||
-    !installmentValue ||
-    !cardNumber ||
-    !cardCvc ||
-    !desiredValue ||
-    !totalLoan ||
-    !installmentId ||
-    !rateTableId ||
-    !contractType
-  ) return res.status(400).send({ message: "Missing props!" })
-
-  api.solicitations.push({
-    id: api.solicitations.length + 1,
-    clientId,
-    installmentInterest,
-    installmentInterestValue,
-    comission,
-    comissionValue,
-    cardExpiration,
-    installmentValue,
-    cardNumber,
-    cardCvc,
-    desiredValue,
-    totalLoan,
-    installmentId,
-    rateTableId,
-    contractType
-  })
-
-  fs.writeFileSync('./src/services/api.json', JSON.stringify(api, null, 2))
-
-  res.status(201).json({ solicitationID: api.solicitations.length })
 }
