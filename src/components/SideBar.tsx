813233a29259b49b39168035e0c34f85316943b8
João Victor <joaovictor-san-co@hotmail.com>
2021-06-03 08:35:23 -0300

    create SideBar component

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import { useContext } from 'react'
+import { SideBarContext } from '../contexts/SideBarContext'
+import styles from '../styles/components/SideBar.module.css'
+
+export function SideBar() {
+  const { open } = useContext(SideBarContext);
+
+  return (
+    <div className={styles.container} style={{ display: open ? 'initial' : 'none' }}>
+      <ul>
+        <li>
+          <Link href="search-client?redirect=solicitations">
+            <a>
+              <div>
+                Solicitações
+              </div>
+            </a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { Header } from '../components/Header'
+import { SideBar } from '../components/SideBar'
 import { LoanProvider } from '../contexts/LoanContext'
+import { SideBarProvider } from '../contexts/SideBarContext'
 
 /* Importa estilos válidos para todo o site */
 import '../styles/fonts.css'
@@ -7,10 +9,13 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <LoanProvider>
-      <Header />
-      <Component {...pageProps} />
-    </LoanProvider>
+    <SideBarProvider>
+      <LoanProvider>
+        <Header />
+        <SideBar />
+        <Component {...pageProps} />
+      </LoanProvider>
+    </SideBarProvider>
   )
 }
 
